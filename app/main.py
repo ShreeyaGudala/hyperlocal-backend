3db29094a6324e38164d911910c14d2a6ef2ac4a
ShreeyaGudala2513 <24211A72B6@bvrit.ac.in>
2026-03-03 07:36:39 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,32 +1,39 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+
 from . import models, schemas, auth
-from .database import engine, SessionLocal, Base
+from .database import engine, get_db, Base
 
 app = FastAPI()
 
+# Create tables
 Base.metadata.create_all(bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 @app.get("/")
 def root():
-    return {"message": "Hyperlocal Service Discovery API is running"}
+    return {"message": "Hyperlocal Backend Running"}
 
-@app.post("/signup", response_model=schemas.UserResponse)
+@app.post("/signup", response_model=schemas.UserResponse, status_code=status.HTTP_201_CREATED)
 def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    # Check duplicate email
+    existing_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered"
+        )
+
     hashed_pw = auth.hash_password(user.password)
+
     new_user = models.User(
         email=user.email,
         phone=user.phone,
         hashed_password=hashed_pw
     )
+
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
+
     return new_user
