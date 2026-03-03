74b58774f031e9c965d342c4ae3bd5cc89ac1147
ShreeyaGudala2513 <24211A72B6@bvrit.ac.in>
2026-03-03 06:50:10 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from . import models, schemas, auth
+from .database import engine, SessionLocal, Base
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def root():
     return {"message": "Hyperlocal Service Discovery API is running"}
+
+@app.post("/signup", response_model=schemas.UserResponse)
+def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    hashed_pw = auth.hash_password(user.password)
+    new_user = models.User(
+        email=user.email,
+        phone=user.phone,
+        hashed_password=hashed_pw
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
