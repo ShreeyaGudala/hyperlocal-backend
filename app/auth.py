111735b394f1d0b354464a40d049563d35bf9a3f
ShreeyaGudala2513 <24211A72B6@bvrit.ac.in>
2026-03-03 08:21:01 +0000

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -5,7 +5,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def hash_password(password: str):
     password = password.strip()
     password = password[:72]   # bcrypt limit safety
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 def verify_password(plain_password: str, hashed_password: str):
     plain_password = plain_password.strip()
